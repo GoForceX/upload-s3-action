670f8f85dde95bfda2f4f716717b2826ef13ba12
GoForceX <67143590+GoForceX@users.noreply.github.com>
2026-03-27 11:49:25 +0000

    cjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,22 @@
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { isBuiltin } from "node:module";
 
 const config = {
     input: "index.js",
     output: {
-        esModule: true,
-        dir: "dist",
-        format: "es",
+        file: "dist/index.cjs",
+        format: "cjs",
         sourcemap: true,
+        exports: "auto",
+        inlineDynamicImports: true
     },
-    plugins: [commonjs(), json(), nodeResolve({ preferBuiltins: true })],
+    plugins: [nodeResolve({
+        preferBuiltins: true,
+    }),
+    commonjs({ignore: (id) => isBuiltin(id) || id.startsWith('node:')}),
+    json()],
 };
 
 export default config;
